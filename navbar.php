6ee3a81cd68cdddc5e4db8ff5d501927929b6888
Mustapha Shehu <86893619+musteey06@users.noreply.github.com>
2021-07-16 11:20:19 +0100

    Add files via upload
    Navbar

---
+++ navbar.php
@@ -0,0 +1,33 @@
+<body>
+
+
+<section class="navbar custom-navbar navbar-fixed-top" role="navigation" href="navbar.php">
+<div id="wrapper">
+<div class="container">
+<nav class="navbar navbar-expand-lg  navbar-light bg-gradient-primary sidebar sidebar-dark accordion ">
+  
+    
+      <a class="navbar-brand" href="#">
+      <img src="image/images.jpeg">STUDENT MANAGEMENT SYSTEM</a>
+    <button class="navbar-toggler" type="button"
+    data-bs-toggle="collapse"
+    data-bs-target="#toggleMobileMenu"
+    aria-controls="toggleMobileMenu"
+    aria-expanded="false"
+    aria-label="Toggle navigation">
+   
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="toggleMobileMenu" >
+    <ul class="navbar-nav ms-auto text-center" >
+    <li ><a class="nav-link" href="index.php">Home</a></li>
+      <li><a class="nav-link" href="registration.php">Registration</a></li>
+      <li><a class="nav-link"  href="records.php">Records</a></li>
+      <li><a class="nav-link"  href="edit.php">Edit Records</a></li>
+      <li><a class="nav-link"  href="about-us.php">About Us</a></li>
+      <li><a class="nav-link"  href="contact.php">Contact</a></li>
+      </ul>
+    </div>
+</nav>
+</div>
+</section>
