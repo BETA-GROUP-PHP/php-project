5dcb6fb736d9be6305e68fd18ae223f917d4c8f5
zainabsaniabdullahi <85300885+zainabsaniabdullahi@users.noreply.github.com>
2021-07-17 00:04:08 +0100

    Add files via upload

---
+++ cont.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+$mysqli =new mysqli('localhost', 'root', '', 'group') or die(mysqli_error($mysqli));
+
+
+if (isset($_POST['send'])){
+    
+   
+    $email= '';
+    $massage= '';
+  
+
+
+    $email=$_POST['email'];
+    $massage=$_POST['massage'];
+   
+ 
+    $mysqli ->query ("INSERT INTO mail (email,massage) VALUES('$email','$massage')") or die ($mysqli->error);    
+   
+    $_SESSION['massage']="massage sent!";
+    $_SESSION['msg_type']="success";
+
+    header("location:contact.php");
+} 
+
+
+
+
+?>
