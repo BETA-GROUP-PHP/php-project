e7437f9ba7ee3fe3b44e3cdac5698ad92df51a72
Rabiu05 <86808549+Rabiu05@users.noreply.github.com>
2021-07-17 23:19:37 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -45,4 +45,99 @@
 
 
 </div>
+<div class="col-md col-lg-3 col-xl-3 mx-auto mt-3">
+<h5 class="text-uppercase mb-4 font-weight-bold text-info">Contact</h5>
+<p>
+<i class="fas fa-home mr-3"></i>Federal Uni, DUT 9556, NG
+</p> 
+<p>
+<i class="fas fa-envelope mr-3"></i>Betagroups@fud.com
+</p> 
+<p>
+<i class="fas fa-phone mr-3"></i>+234 7066907623
+</p> 
+</div>
 
+<hr class="mb-4"> 
+<div class="row align-items-center">
+
+<div class="col-md-7 col-lg-8">
+<p>
+copyright &copy; 2021 All rights reserved by:
+<a href="#" style="text-decoration:none;">  
+<strong class="text-info" > BetaGroup</strong>
+</a>
+</p>
+
+</div>
+<div class="col-md-5 col-lg-4">
+
+<div class="text-center text-md-right" >
+<ul class="list-unstyled list-inline">
+<li class="list-inline-item">
+
+<a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"
+><i class="fab fa-facebook"></i></a>
+
+<a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"
+><i class="fab fa-twitter"></i></a>
+
+
+<a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"
+><i class="fab fa-instagram"></i></a>
+
+
+<a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"
+><i class="fab fa-google"></i></a>
+
+
+<a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"
+><i class="fab fa-youtube"></i></a>
+
+</li>
+
+</ul>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+
+
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+crossorigin="anonymous"></script>
+  
+
+</body>
