15ecb07a7613a25d7b1fb4be26784c8bd1d0869b
hbulangu <87013784+hbulangu@users.noreply.github.com>
2021-07-17 16:41:29 +0100

    Add files via upload

---
+++ process.php
@@ -0,0 +1,68 @@
+
+<?php
+session_start();
+
+
+
+
+$mysqli =new mysqli('localhost', 'root', '', 'group') or die(mysqli_error($mysqli));
+$id=0;
+$update = false;
+$name= '';
+$email= '';
+$gender= '';
+$course= '';
+$level= '';
+$phone= '';
+
+    if (isset($_POST['save'])){
+    
+    
+
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $gender=$_POST['gender'];
+    $course=$_POST['course'];
+    $level=$_POST['level'];
+    $phone=$_POST['phone'];
+ 
+    $mysqli ->query ("INSERT INTO data (name,email,gender,course,level,phone) VALUES('$name','$email','$gender','$course','$level','$phone')") or die ($mysqli->error);    
+   
+    $_SESSION['massage']="Record has been saved!";
+    $_SESSION['msg_type']="success";
+
+    header("location:registration.php");
+} 
+
+
+
+if(isset($_GET['delete'])){
+
+$id=$_GET['delete'];
+$mysqli ->query ("DELETE FROM data WHERE id=$id") or die($mysqli->error());
+$_SESSION['massage']="Record has been deleted!";
+$_SESSION['msg_type']="danger";
+
+header("location:edit.php");
+}
+
+
+if(isset($_GET['edit'])){
+
+    $id= $_GET['edit'];
+    $update=true;
+    $sql=("SELECT * FROM data WHERE id =$id") ;
+    $result=$mysqli->query($sql);
+if($result->num_rows>0){
+        $row = $result->fetch_array();
+
+        $name=$row['name'];
+        $email=$row['email'];
+        $gender=$row['gender'];
+        $course=$row['course'];
+        $level=$row['level'];
+        $phone=$row['phone'];
+       
+}
+}
+
