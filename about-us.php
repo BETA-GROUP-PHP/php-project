888254a7f05e33a88f7febf0bdbf3f4459d0768e
Joma60 <86953240+Joma60@users.noreply.github.com>
2021-07-18 19:26:41 -0400

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,75 @@
+<?php
+include 'head.php';
+include 'navbar.php';
+?>
+
+<section class="section-background">
+          <div class="container">
+               <div class="row">
+                    <div class="col-md-offset-1 col-md-4 col-xs-12 pull-right">
+                         <img src="images/about-1-720x720.jpg" class="img-responsive img-circle" alt="">
+                    </div>
+
+                    <div class="col-md-7 col-xs-12">
+                         <div class="about-info">
+                              <h2>OPHANAGES:</h2>
+
+                              <figure>
+                                   <figcaption>
+                                        <p>As part of the Company’s Corporate Social Responsibility Scheme area of intervention, funds are approved to buy and distribute welfare items to orphanages, ranging from food items, toiletries, laundries etc. Items are bought and distributed to the following orphanages:
+1.    Abuja Children Home, Karu, Abuja
+2.    Kuje Prison, Kuje, Abuja
+3.    Jamaiyar Matan Arewa, Kaduna</p>
+
+                                       
+                                   </figcaption>
+                              </figure>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </section>
+
+     <section>
+          <div class="container">
+               <div class="row">
+                   
+
+                    <div class="col-md-offset-1 col-md-7 col-xs-12">
+                         <div class="about-info">
+                              <h2>Company Background</h2>
+
+                              <figure>
+                                   <figcaption>
+                                        <p>BETA GROUP  company engaged in Consultancy, School management System. The company believes in efficient, honest and qualitative service to the customer. Using the latest technology and employing self-motivated workers, the company’s objective is to grow steadily into a world class, integrated Technology company.</p>
+
+                                        <p>The company believes that its workers are the most valuable and dependable asset, to meet the challenges of its objectives. While the company will strive to create a congenial and harmonious working environment, offer equal opportunity for advancement and reward workers as appropriate, it will also demand dedication to service and loyalty from employees.</p>
+                                   </figcaption>
+                              </figure>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </section>
+
+     <section class="section-background">
+          <div class="container">
+               <div class="row">
+                    <div class="col-md-12 col-sm-12">
+                         <div class="text-center">
+                              <h2>Qoutes To Live By Quotes</h2>
+
+                              <br>
+
+                              <p class="lead">“Life is fragile and temporary. The faces of today quickly become the faces of the past. Sorrow, pain, and anger... it all fades-
+except love. Love is forever and there after, even when we've fallen to our graves.”
+― Lee Argus</p>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </section>
+     <?php
+include 'footer.php';
+
+?>
